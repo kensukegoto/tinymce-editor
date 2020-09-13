052f16507dd411b5523e8dff5060e4064c2d711f
kensuke goto <kensuke.goto.1981@gmail.com>
2020-09-13 12:38:05 +0900

    追加：座布団を敷ける

--- _dev/script/index.js
+++ _dev/script/index.js
@@ -31,7 +31,6 @@ function addTextArea(type,content){
   if(type && content){
     textarea.value = `<${type}>${content}</${type}>`;
   }
-  
 
   section.appendChild(textarea);
 
@@ -42,7 +41,7 @@ function addTextArea(type,content){
     plugins: 'paste',
     paste_as_text: true,
     contextmenu: false,
-    forced_root_block: false,
+    // forced_root_block: false,　// 改行をpとbrどちらにするか
     menubar: false,
     height: 500,
     // contextmenu_never_use_native: true,
@@ -50,18 +49,61 @@ function addTextArea(type,content){
       body { font-family:Helvetica,Arial,sans-serif; font-size:14px }
       .left { text-align: left; }
       .bold { font-weight: bold; } 
+      .bgBlue { padding: 8px; background-color: #f5e9f7 }
+      .bgRed { padding: 8px; background-color: #fee5e5 }
       `,
     formats: {
       bold: { inline: 'span', classes: 'bold' },
       para: { block: 'p', classes: 'para'},
+      bgBlue : { selector: 'p', classes: 'bgBlue'},
+      bgRed : { selector: 'p', classes: 'bgRed'},
     },
-    toolbar: 'undo redo | para bold forecolor',
+    toolbar: 'undo redo | para bold forecolor | textBg alignment',
     color_map: [
       '333333', 'Black',
       'FF0000', 'Red',
       '0000FF', 'Blue',
     ],
     custom_colors: false,
+    setup: function(editor){
 
+      editor.ui.registry.addIcon('bgBlue',`<svg width="24" height="24"><rect x="3" y="3" width="18" height="18" rx="1" fill-rule="evenodd" fill="#f5e9f7"></rect></svg>`);
+      editor.ui.registry.addIcon('bgRed',`<svg width="24" height="24"><rect x="3" y="3" width="18" height="18" rx="1" fill-rule="evenodd" fill="#fee5e5"></rect></svg>`);
+
+      editor.ui.registry.addMenuButton('textBg', {
+        // text: '座布団',
+        icon: 'fill',
+        fetch: function (callback) {
+          var items = [
+            {
+              type: 'menuitem',
+              text: '青',
+              icon: 'bgBlue',
+              onAction: function (_) {
+                editor.formatter.remove('bgRed');
+                editor.formatter.toggle('bgBlue');
+              },
+              onSetup: function(api) {
+                return function() {};
+              }
+            },
+            {
+              type: 'menuitem',
+              text: '赤',
+              icon: 'bgRed',
+              onAction: function (_) {
+                editor.formatter.remove('bgBlue');
+                editor.formatter.toggle('bgRed');
+              },
+              onSetup: function(api) {
+                return function() {};
+              }
+            }
+          ];
+          callback(items);
+        }, // fetch
+      });
+
+    }
   });
 }
