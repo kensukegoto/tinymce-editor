cef2be38132574e2ee76cfb21125be8191ceff3b
kensuke goto <kensuke.goto.1981@gmail.com>
2020-09-16 16:02:07 +0900

    追加：中タイトル・小タイトルを追加出来る

--- _dev/script/index.js
+++ _dev/script/index.js
@@ -169,20 +169,53 @@ function addTitleArea(list){
 
   const len = form.querySelectorAll("input[type='text'],textarea").length;
 
-  const section = document.createElement("section");
-  const textarea = document.createElement("input");
-  textarea.setAttribute("id",`textarea_${len + 1}`);
-  textarea.setAttribute("type","text");
-
   let value = "";
 
   if(list){
-    value += list;
+    value += list.content;
   }
 
-  textarea.value = value;
+  const flagment = document.createElement("div");
 
-  section.appendChild(textarea);
+  flagment.innerHTML =  `
+    <section>
+      <div class="radio__box">
+        <input type="radio" name="radio_${len + 1}" value="h2">中
+        <input type="radio" name="radio_${len + 1}" value="h3">小
+      </div>
+      <input id="textarea_${len + 1}" data-id="${len + 1}" type="text" value="${value}">
+    </section>`;
 
-  form.appendChild(section);
+  form.appendChild(flagment.querySelector("section"));
+
+  const radios = document.querySelectorAll(`input[name='radio_${len + 1}']`);
+  const text = document.querySelector(`#textarea_${len + 1}`);
+
+  radios.forEach(radio => {
+
+    radio.addEventListener("change",()=>{
+      radios.forEach(target => {
+        if(target.checked){
+          text.classList.add(target.value)
+        }else {
+          text.classList.remove(target.value)
+        }
+      })
+    })
+
+  });
+
+  radios.forEach((radio,idx) => {
+    if(list){
+      if(radio.value === list.tag) radio.click()
+    } else {
+      if(idx === 0) {
+
+        radio.click()
+      }
+    };
+  })
+
+
+
 }
